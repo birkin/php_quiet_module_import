3b6677b949385a19902859b0f45fbb528f3463bc
birkin <birkin.diana@gmail.com>
2013-09-20 12:41:52 -0400

    found credit-source.

--- file_a.php
+++ file_a.php
@@ -8,6 +8,7 @@
   One use-case for this feature: testing.
     A script can be accessed to test its classes & functions without executing non-targeted code.
   This is a way to achieve similar functionality in PHP.
+  ( based on <http://stackoverflow.com/questions/2413991/php-equivalent-of-pythons-name-main> )
 */
 
 define( 'DIRECT_FROM_APACHE', !debug_backtrace() );
